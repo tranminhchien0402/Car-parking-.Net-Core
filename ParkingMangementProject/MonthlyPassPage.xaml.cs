2d35bbe68bb3e88864d31379fd056b64b6177cca
tranminhchien0402 <tranminhchien20050402@gmail.com>
2026-05-16 17:08:50 +0700

    Update lan 2

--- ParkingMangementProject/MonthlyPassPage.xaml.cs
+++ ParkingMangementProject/MonthlyPassPage.xaml.cs
@@ -85,7 +85,7 @@ namespace ASM
 
             if (plate.Length < 8 || plate.Length > 9)
             {
-                ShowMessage("Biển số phải từ 8 đến 9 ký tự (VD: 29A-12345).");
+                ShowMessage("lorem is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions.");
                 return false;
             }
 
